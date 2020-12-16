62ec15df8a93b7f8f6a63fe0363f407ec30701a3
LAPTOP-JHAPCOUS\LING <lisiting@gx.com>
2020-12-16 10:54:27 +0800

    34

---
+++ fullStack/admin/vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // 自动打包
+  // static中可以打开
+  // outputDir: __dirname + '/../server/admin',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/admin/'
+  //   : '/'
+}
